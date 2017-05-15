3d94705d48874ff52648116665eeed47373df1b4
Legbon <leogbondoc@gmail.com>
2017-05-15 15:31:38 +0800

    Created a MyPDO class.. code kinda stolen from stack overflow. Hid db_settings I use in gitignore and added a sample. Moved index to public.

---
+++ classes/MyPDO.php
@@ -0,0 +1,19 @@
+<?php
+class MyPDO extends PDO
+{
+	public $dbname;
+    public function __construct($file = '../config/db_settings.ini')
+    {
+        if (!$settings = parse_ini_file($file, TRUE)) throw new exception('Unable to open ' . $file . '.');
+       
+        $dsn = $settings['database']['driver'] .
+        ':host=' . $settings['database']['host'] .
+        ((!empty($settings['database']['port'])) ? (';port=' . $settings['database']['port']) : '') .
+        ';dbname=' . $settings['database']['schema'];
+
+        $this->dbname = $settings['database']['schema'];
+       
+        parent::__construct($dsn, $settings['database']['username'], $settings['database']['password']);
+    }
+}
+?>
