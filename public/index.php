05182d48fbc4095126c685d357744acb91350286
Legbon <leogbondoc@gmail.com>
2017-05-18 16:40:45 +0800

    Added links to js files in index.

--- public/index.php
+++ public/index.php
@@ -14,6 +14,8 @@
 <head>
 	<title> MyTodos </title>
 	<link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.min.css" />
+	<script type="text/javascript" src="./assets/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="./assets/js/bootstrap.min.js"></script>
 </head>
 <body>
 
